4f52243600a7fcbf076d7d76a3ba28bf932fbbc1
Grigory <grromanov.for.training@yandex.ru>
2024-11-19 20:19:10 +0300

    All over again

--- src/Main.java
+++ src/Main.java
@@ -1,40 +1,6 @@
-import task.tracker.*;
-import manager.Manager;
-
 public class Main {
 
     public static void main(String[] args) {
-        //TODO нужно заменить ключ в мапе с hashCode UUID - сформированный для каждого объектах см. телегу
-        Manager manager = new Manager();
-        Task task1 = new Task("Переезд!!");
-        Task task2 = new Task("Переезд");
-        Epic epic1 = new Epic("Сборка пк");
-        Epic epic2 = new Epic("Переезд");
-
-
-        Subtask subtask1 = manager.addSubtaskInEpic(epic1, "Купить запчасти");
-        Subtask subtask2 = manager.addSubtaskInEpic(epic1, "Купить запчасти");
-        Subtask subtask3 = manager.addSubtaskInEpic(epic1, "Купить моник");
-
-        manager.addTask(task1);
-        manager.addTask(epic1);
-        //manager.deleteById(task1.getTaskStatus(),task1.hashCode());
-        //System.out.println(manager.getListTasks());
-
-        System.out.println(task1.equals(epic2));
-
-        System.out.println(task1.hashCode());
-        System.out.println(task2.hashCode());
-        System.out.println(epic1.hashCode());
-        System.out.println(epic2.hashCode());
-
-        //System.out.println(manager.getListEpicSubtasks(TaskStatus.NEW, epic1.hashCode()));
-        //System.out.println(manager.getTaskById(TaskStatus.NEW, -280801830));
-
-        manager.setStatusTask(TaskStatus.DONE, subtask1);
-        //manager.setStatusTask(TaskStatus.IN_PROGRESS, task1);
-        System.out.println(manager.getListTasks());
-
-
+        System.out.println("Все сначала)");
     }
 }
--- src/task/tracker/TaskStatus.java
+++
@@ -1,7 +0,0 @@
-package task.tracker;
-
-public enum TaskStatus {
-    NEW,
-    IN_PROGRESS,
-    DONE
-}
