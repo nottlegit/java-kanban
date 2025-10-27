b2159f160e1b38bf35f3c06d3f96670a87686fb0
Grigoriy(notebook) <grrom.for.training@yandex.ru>
2025-10-27 14:07:11 +0300

    refactor: Заменить тестовы сервер на HttpTaskServer

--- test/api/handlers/HttpTaskServerTest.java
+++ test/api/handlers/HttpTaskServerTest.java
@@ -1,45 +1,34 @@
 package api.handlers;
 
-import com.sun.net.httpserver.HttpServer;
+import api.HttpTaskServer;
 import managers.TaskManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import util.Managers;
 
 import java.io.IOException;
-import java.net.InetSocketAddress;
 import java.net.http.HttpClient;
 
 class HttpTaskServerTest {
     private TaskManager manager;
-    private final int TEST_PORT = 8081;
-    protected HttpServer server;
+    protected HttpTaskServer server;
     protected HttpClient client;
 
     @BeforeEach
     void setUp() throws IOException {
         manager = Managers.getDefault();
-        server = HttpServer.create(new InetSocketAddress(TEST_PORT), 0);
-
-        server.createContext("/tasks", new TaskHandler(manager));
-        server.createContext("/subtasks", new SubtaskHandler(manager));
-        server.createContext("/epics", new EpicHandler(manager));
-        server.createContext("/history", new HistoryHandler(manager));
-        server.createContext("/prioritized", new PrioritizedHandler(manager));
-
-        server.start();
+        server = new HttpTaskServer(manager);
+        server.startServer();
         client = HttpClient.newHttpClient();
     }
 
     @AfterEach
     void tearDown() {
-        if (server != null) {
-            server.stop(0);
-        }
+        server.stop();
     }
 
     protected String getBaseUrl() {
-        return "http://localhost:" + TEST_PORT;
+        return "http://localhost:8080";
     }
 
     public TaskManager getManager() {
